a7ed6931ae3c3068576cbf5d4f50d914f08781f6
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-09-08 00:15:50 +0600

    sgsg

---
+++ pp1.js
@@ -0,0 +1,6 @@
+const num =[2,4,5,8,9]
+
+for (let i=0; i < num.length;i++){
+  const res=num[i]
+  console.log( i ,res)
+}
---
+++ pp2.js
@@ -0,0 +1,6 @@
+function sum(num1,num2){
+  const result =num1+num2;
+  return result;
+}
+const result=sum(10,5)
+console.log(result)
