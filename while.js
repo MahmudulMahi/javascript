5b8a2ec4103cf4ee55ccbbfc1fd58fa407470ab0
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-09-10 00:45:15 +0600

    sghjkl

--- while.js
+++ while.js
@@ -4,3 +4,12 @@ while(rostgiven <10000){
   console.log('den');
   rostgiven++;
 }
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//  while loop
+ let j= 0;
+ while(j < arr.length)
+ {
+     console.log(arr[j]);
+     j++;
+ }
